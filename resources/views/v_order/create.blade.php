286e0339924e9bb2d8ac44f19cffcdbbae7ea55a
Lenxza1 <naufal.rara55@gmail.com>
2024-12-17 08:21:27 +0700

    Add product images and update order creation form to display product prices

--- resources/views/v_order/create.blade.php
+++ resources/views/v_order/create.blade.php
@@ -6,13 +6,33 @@
         <form method="post" action="{{route('order.store')}}">
             @csrf
             <div class="form-group mb-4">
-                <label for="exampleFormControlSelect1" class="form-label">Nama Produk</label>
-                <select class="form-control" id="exampleFormControlSelect1" name="idProduct">
+                <label for="productSelect" class="form-label">Nama Produk</label>
+                <select class="form-control" id="productSelect" name="idProduct">
                     @foreach ($products as $product)
-                        <option value="{{$product->id}}">{{$product->name}}</option>
+                        <option value="{{ $product->id }}" data-price="{{ $product->price }}">
+                            {{ $product->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
+            <div class="mb-3">
+                <label for="productPrice" class="form-label">Harga Buah</label>
+                <input type="number" class="form-control" id="productPrice" placeholder="Masukan Jumlah Pesanan" readonly value="">
+            </div>
+
+            <script>
+                document.getElementById('productSelect').addEventListener('change', function() {
+                    var selectedOption = this.options[this.selectedIndex];
+                    var price = selectedOption.getAttribute('data-price');
+                    document.getElementById('productPrice').value = price;
+                });
+
+                window.addEventListener('DOMContentLoaded', (event) => {
+                    var select = document.getElementById('productSelect');
+                    var event = new Event('change');
+                    select.dispatchEvent(event);
+                });
+            </script>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Jumlah Pesanan</label>
                 <input type="number" class="form-control" id="exampleInputPassword1" name="amount" placeholder="Masukan Jumlah Pesanan">
